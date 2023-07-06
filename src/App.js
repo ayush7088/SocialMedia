86150d565c307c58bcbba469b1e3b174b904d29d
ayush7088 <ayushjoshi282@gmail.com>
2023-07-06 22:40:08 +0530

    Fix: minor changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css"
+import Home from "./pages/home/Home";
 function App() {
   return (
     <div className="App">
-        hey world this is first sentence.....
+        <div className="blur" style = {{top: '-18%',right:'0'}}></div>
+        <div className="blur" style = {{top: '36%',left:'-8rem'}}></div>
+        <Home />  
     </div>
+
+    //  <div className="backchodi">hi</div>
   );
 }
 
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div className="Home">
+        <div className="profileSide">Profile</div>
+        <div className="postSide">Posts</div>
+        <div className="RigthtSide">RightSide</div>
+
+    </div>
+  )
+}
+
+export default home
