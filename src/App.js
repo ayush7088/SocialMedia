9225c3ae4e1f3d70cab2e57d00edc23ee93de5f9
ayush7088 <ayushjoshi282@gmail.com>
2023-07-06 19:04:56 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
-        <div className = "divBar"></div>
+        hey world this is first sentence.....
     </div>
   );
 }
