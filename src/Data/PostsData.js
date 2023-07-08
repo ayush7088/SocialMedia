dd081455f44d50408c58371468c6c9a79be20c81
ayush7088 <ayushjoshi282@gmail.com>
2023-07-08 21:24:32 +0530

    feat: added more UI

---
+++ src/Data/PostsData.js
@@ -0,0 +1,28 @@
+import postPic1 from '../img/postpic1.jpg';
+import postPic2 from '../img/postpic2.jpg';
+import postPic3 from '../img/postpic3.JPG';
+
+
+export const PostsData = [
+    {
+        img: postPic1,
+        name: 'Alisha',
+        desc: 'This is my first Post here! #NewPost2023',
+        likes: 102,
+        liked: true
+    },
+    {
+        img: postPic2,
+        name: 'Natasha',
+        desc: 'Party Time :)',
+        likes: 99,
+        liked: false
+    },
+    {
+        img: postPic3,
+        name: 'Emma',
+        desc: 'At Festival!!',
+        likes: 212,
+        liked: false
+    }
+]
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ProfileSide from '../../components/profileSide/ProfileSide';
 import './Home.css';
 import PostSide from '../../components/PostSide/PostSide';
+import RightSide from '../../components/RightSide/RightSide';
 
 const home = () => {
   return (
     <div className="Home">
         <ProfileSide />
         <PostSide />
-        <div className="RigthtSide">RightSide</div>
+        <RightSide />
 
     </div>
   )
